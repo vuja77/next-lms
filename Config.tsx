c0fcb5a058b371ea95585b13948640bf598152b6
Vuja <djordjijevujovic210306@gmail.com>
2023-12-31 02:20:49 +0100

    fix2

--- Config.tsx
+++ Config.tsx
@@ -1,4 +1,4 @@
 export const Config = {
-    apiUrl: "https://lms-next.work.gd/api/index.php",
-    storageUrl: "https://api.lms-next.work.gd/ets-site-backend/storage/app/public"
+    apiUrl: "https://api.lms-next.work.gd/api/index.php",
+    storageUrl: "https://lms-next.work.gd/ets-site-backend/storage/app/public"
 } as const;
